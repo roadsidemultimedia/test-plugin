98fb95622ee3f552151684ee66db88b9d1f75807
FaceLord <milo@milojennings.com>
2014-07-16 17:43:37 -0700

    another typo fix

--- test-plugin.php
+++ test-plugin.php
@@ -6,7 +6,7 @@ Version:           1.0.1
 Text Domain:       events-calendar-category-colors
 License:           GNU General Public License v2
 License URI:       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-GitHub Plugin URI: https://github.com/github.com/roadsidemultimedia/test-plugin
+GitHub Plugin URI: https://github.com/roadsidemultimedia/test-plugin
 GitHub Branch:     master
 */
 
