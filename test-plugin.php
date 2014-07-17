e55658486e8a091b61c33401f982edaf8652336e
Milo Jennings <milotalonjennings@gmail.com>
2014-07-16 17:40:14 -0700

    new version

---
+++ test-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:       Test Plugin
+Plugin URI:        https://github.com/https://github.com/roadsidemultimedia/test-plugin
+Version:           1.0.1
+Text Domain:       events-calendar-category-colors
+License:           GNU General Public License v2
+License URI:       http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+GitHub Plugin URI: https://github.com/github.com/roadsidemultimedia/test-plugin
+GitHub Branch:     master
+*/
+
+$test_var_9000 = "1.0.1";
